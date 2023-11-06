27556ddd969ff1ea5453ee875559cfd2ae614c49
lodhikomal <komallodhi720@gmail.com>
2023-11-06 18:45:49 +0530

    css

--- src/component/AdminPlanner.js
+++ src/component/AdminPlanner.js
@@ -5,42 +5,45 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideo } from "@fortawesome/free-solid-svg-icons";
 import { faListUl } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-
 function AdminPlanner() {
-  const [active, setActive] = useState(null);
+  const [toggle, setToggle] = useState(false);
 
-  const handleClick = (btnId) => setActive(btnId);
+  const handleClick = () => setToggle(!toggle);
+
   return (
     <div>
-      <div className="admin-container">
-        <div className="admin-menu">
+      <div className="admin-menu">
+        <div className="admin-container">
           <div className="admin-logo">
             <img src={adminLogo} alt="logo" className="logo-img"></img>
             <h3 className="logo-text">Disney+hotstar</h3>
           </div>
-          <div className="button-container">
+          <div className="menu-icon-bars" onClick={handleClick}>
+            {toggle ? (
+              <FontAwesomeIcon icon={faXmark} className="menu-icon" />
+            ) : (
+              <FontAwesomeIcon icon={faBars} className="menu-icon" />
+            )}
+          </div>
+        </div>
+        <div className="button-container">
+          <>
             <NavLink to={"/"} className="navlink">
-              <button
-                type="button"
-                className={`movie-btn ${!active == 1 ? "active" : ""}`}
-                onClick={() => handleClick(1)}
-              >
+              <button type="button" className="movie-btn">
                 <FontAwesomeIcon icon={faVideo} className="icon" />
                 <span className="movie-text"> Movies </span>
               </button>
             </NavLink>
             <NavLink to={"/category"} className="navlink">
-              <button
-                type="button"
-                className={`movie-btn ${!active == 2 ? "active" : ""}`}
-                onClick={() => handleClick(2)}
-              >
+              <button type="button" className="movie-btn">
                 <FontAwesomeIcon icon={faListUl} className="icon" />
                 <span className="movie-text">Category</span>
               </button>
             </NavLink>
-          </div>
+          </>
         </div>
       </div>
     </div>
--- src/component/Movie.js
+++ src/component/Movie.js
@@ -5,17 +5,27 @@ import "../style/movieStyle.css";
 function Movie() {
   return (
     <Layout>
-      <div className="table-container">
-        <table>
+      <div className="table-heading">
+        <div className="table-up">
+          <h3>Movie Panel</h3>
+
+          <input
+            type="text"
+            className="searchbar"
+            placeholder="Search.."
+          ></input>
+        </div>
+        <table className="table-container">
+          <thead>
+            <tr className="table-row">
+              <th>Name</th>
+              <th>movies.name</th>
+              <th>Release date</th>
+              <th>type</th>
+            </tr>
+          </thead>
           <tbody>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>movies.name</th>
-                <th>Release date</th>
-                <th>type</th>
-              </tr>
-            </thead>
+            <td></td>
           </tbody>
         </table>
       </div>
